40f3d38d8f0d08a7256827465f9c8603d34c7dc7
hickup <hickup089@163.com>
2021-11-10 10:23:44 +0800

    22/10 早上SpringSecurityDemo

---
+++ src/main/java/com/example/springsecuritydemo01/SpringsecurityDemo01Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritydemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityDemo01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecuritydemo01/SpringsecurityDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritydemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
